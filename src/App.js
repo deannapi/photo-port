c46b0393f8f60dd31eeeb089316492d830316050
deannapi <deannapi.mart@gmail.com>
2020-07-28 08:20:05 -0500

    added gallery

--- src/App.js
+++ src/App.js
@@ -1,71 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Nav from './components/Nav';
 import About from './components/About';
-import coverImage from "../../assets/cover/cover-image.jpg";
-import './App.css';
-
-const categories = [
-  {
-    name: "commercial",
-    description:
-      "Photos of grocery stores, food trucks, and other commercial projects",
-  },
-  { name: "portraits", description: "Portraits of people in my life" },
-  { name: "food", description: "Delicious delicacies" },
-  {
-    name: "landscape",
-    description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-  },
-];
+import Gallery from './components/Gallery';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
       <main>
-        <About></About>
+        <div>
+          <Gallery></Gallery>
+          <About></About>
+        </div>
       </main>
     </div>
   );
-};
-
-function Nav() {
-
-  return (
-    <header>
-      <h2>
-        <a href="/">
-          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
-        </a>
-      </h2>
-      <nav>
-        <ul className="flex-row">
-          <li className="mx-2">
-            <a href="#about">
-              About me
-            </a>
-          </li>
-          <li>
-            <span>Contact</span>
-          </li>
-          {categories.map((category) => (
-            <li
-              className="mx-1"
-              key={category.name}
-            >
-              <span onClick={categorySelected} >
-                {category.name}
-              </span>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
-  );
 }
 
-function categorySelected() {
-  console.log("hello")
-}
-
-export default Nav;
 export default App;
