3c2d26b7505aa1e75e778230dd115404eb3fcebf
deannapi <deannapi.mart@gmail.com>
2020-07-30 14:34:14 -0500

    switched lines in app to get website to work

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import About from './components/About';
 import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
-const [contactSelected, setContactSelected] = useState(false);
-
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
   const [categories] = useState([
     {
       name: 'commercial',
@@ -18,25 +17,25 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
   return (
     <div>
       <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
-        contactSelected={contactSelected}
-        setContactSelected={setContactSelected}
-      ></Nav>
+      categories={categories}
+      setCurrentCategory={setCurrentCategory}
+      currentCategory={currentCategory} 
+      contactSelected = {contactSelected}
+      setContactSelected= {setContactSelected}
+      >
+      </Nav>
       <main>
-          {!contactSelected ? (
-            <>
-              <Gallery currentCategory={currentCategory}></Gallery>
-              <About></About>
-            </>
-          ) : (
-              <ContactForm></ContactForm>
-          )}
+      {!contactSelected ? (
+     <>
+      <Gallery currentCategory={currentCategory}></Gallery>
+      <About></About>
+    </>
+      ) : (
+         <ContactForm></ContactForm>
+       )}
       </main>
     </div>
   );
